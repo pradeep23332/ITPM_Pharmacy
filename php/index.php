ede5babe88f75fdc28edbb696083349b9a123c39
yuthishka14 <it20140748@my.sliit.lk>
2022-03-27 14:55:23 +0530

    added admin login

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <tittle>My Website</tittle>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>This is the index page</h1>
+    <br>
+    Hello, <?php echo $user_data ['user_name']; ?>
+</br>
+</body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+include("connection.php");
+include("functions.php");
+
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+    //something was posted
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    if(! empty($user_name)&& !empty($password)&& !is_numeric($user_name))
+    {
+        //read from database
+        $query = "select * from users where user_name= '$user_name' limit 1";
+        $result = mysqli_query($con, $query);
+        
+        if($result)
+        {
+            if($result && mysqli_num_rows($result) >0)
+        {
+          $users_data = mysqli_fetch_assoc($result);
+          if($users_data['password'] === $password  )
+          {
+            $_SESSION['user_id'] = $users_data['user_id'];
+              header("Location: Admin_home.php");
+              die;
+          }
+         }
+        }
+        echo "Wrong Username Or Password !" ;
+    }else
+    {
+       echo "Wrong Username Or Password ! !" ;
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <link rel="stylesheet" href="../css/login1.css"> 
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />  
+</head>
+<body>
+ <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">HEALTH CARE</label>
+     
+      <ul>
+        <li><a style="text-decoration: none" class="text-light" href="#">Home</a></li>
+        <li><a style="text-decoration: none" class="text-light" href="#">About Us</a></li>
+		 <li><a style="text-decoration: none" class="text-light" href="#">Contact Us</a></li>
+        <li><a style="text-decoration: none" class="text-light" href="#">Online Pharamacy Services</a></li>
+        <li><a style="text-decoration: none" class="text-light" href="login.php">Admin Login</a></li>
+      </ul>
+    </nav>
+	
+    
+    <style type="text/css"> 
+        
+        #box{
+            background-color: gray;
+            margin: auto;
+            width: 500px;
+			height:350px;
+            padding: 20px;
+			margin-top:150px;
+		
+        }
+
+    </style>
+
+    <div id="box">
+        <form method="post">
+            <div style="font-size: 20px; 
+                        margin: 10px;
+                        color:white;" align="center">Login</div>
+            <lable><b>User Name</b></lable>
+            <input id="Username" class="form-control" type="text" name="user_name"><br><br>
+			<lable><b>Password</b></lable>
+            <input id="Passsword" class="form-control" type="password" name="password"><br><br>
+			
+		    <input class="btn btn-primary" type="submit" value="Login" ><br><br>
+			
+            
+    </form>
+    </div>
+	
+	
+    <div class="footer">
+
+      <div class="strokeme1">
+      <b><u>Hours of Operation</b></u></br>
+      Avaliable from 9AM to 7PM from</br>
+      Monday to Saturaday</br>
+      Order will be delivered within 48</br>
+      hours of orders.</br>
+      </div>
+      
+      
+      <div class="strokeme2">
+        <b><u>Contact Us</b></u></br>
+        Mobile:071 5689745</br>
+        Work:031 5688554</br>
+        Email:healthcare@ph.lk
+      </div>
+      
+      </div>
+	  
+</body>
+</html>
