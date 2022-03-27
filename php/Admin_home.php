4909c7045cd68c9a03dc64e62a599272048e290b
pradeep23332 <it20128654@my.sliit.lk>
2022-03-27 14:27:06 +0530

    added system homepage and admin homepage

---
+++ php/Admin_home.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con);
+?>
+
+<!DOCTYPE html>
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Responsive Navbar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Admin.css">
+    <script src="https://kit.fontawesome.com/dabbda7d23.js" crossorigin="anonymous"></script>
+    <div class="container">
+      
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">HEALTH CARE</label>
+     
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+		 <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Online Pharamacy Services</a></li>
+        <li><a href="logout.php">Log Out</a></li>        
+      </ul>
+    </nav>
+     <!--sidebar start-->
+     <div class="sidebar">
+      <center>
+        <img src="../Images/22.png" class="profile_image" alt="">
+        <h4>Admin</h4>
+      </center>
+      <a href="#"><i class="fa-solid fa-pills"></i><span>Manage Medicines</span></a> 
+      <a href="#"><i class="fa-solid fa-users"></i><span>Manage Staff</span></a> 
+      <a href="#"><i class="fa-solid fa-people-roof"></i><span>Manage Suppliers</span></a>          
+      <a href="#"><i class="fa-solid fa-chart-bar"></i><span>Manage Sales</span></a>    
+      <a href="#"><i class="fa-solid fa-file-prescription"></i><span>Manage Prescriptions</span></a>   
+      
+    </div>
+    <!--sidebar end-->
+    <section></section>
+    
+  </body>
+</html>
---
+++ php/Homepage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Responsive Navbar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+	
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <div class="container">
+      
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">HEALTH CARE</label>
+     
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+		 <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Online Pharamacy Services</a></li>
+        <li><a href="login.php">Admin Login</a></li>
+      </ul>
+    </nav>
+    <section></section>
+    <div class="footer">
+
+      <div class="strokeme1">
+      <b><u>Hours of Operation</b></u></br>
+      Avaliable from 9AM to 7PM from</br>
+      Monday to Saturaday</br>
+      Order will be delivered within 48</br>
+      hours of orders.</br>
+      </div>
+      
+      
+      <div class="strokeme2">
+        <b><u>Contact Us</b></u></br>
+        Mobile:071 5689745</br>
+        Work:031 5688554</br>
+        Email:healthcare@ph.lk
+      </div>
+      
+      </div>
+  </body>
+</html>
