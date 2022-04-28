6ad07e423b410c4152156800da2fe7c11b3778f3
yuthishka14 <it20140748@my.sliit.lk>
2022-04-28 20:25:11 +0530

    Added deleteSales.php file

---
+++ php/deleteSales.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'connect.php';
+if(isset($_GET['deleteid'])){
+     $id=$_GET['deleteid'];
+
+     $sql="delete from crud where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+       // echo"Deleted sucessfull";
+       header('location:displaySales.php');
+    }else{
+        die(mysqli_error($con));
+    }
+}
+
+
+?>
